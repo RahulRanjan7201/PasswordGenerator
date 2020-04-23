dfeaff56202525e79267ed1799ec1431d5dbd097
rahul.ranjan7201@gmail.com <rahul.ranjan7201@gmail.com>
2020-04-23 23:04:33 +0530

    Password Gen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Password-generator';
+  password:string = "";
+  includeLetters:boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false; 
+  passwordLength:number = 0;
+  onButtonClick() {
+    const numbers = '1234567890';
+    const uppercaseLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
+    if(this.includeLetters) {
+      validChars += uppercaseLetter;
+      validChars += lowerCase;
+    }
+    if(this.includeNumbers) {
+      validChars +=numbers;
+    }
+    if(this.includeSymbols) {
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+    for(let i=0;i<this.passwordLength; i++) {
+       const index = Math.floor(Math.random() * validChars.length);
+       generatedPassword +=  validChars[index];
+    }     
+    this.password =generatedPassword;
+  }
+  getPassword() {
+    return this.password;
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = ! this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onChangeLength(value: string) {
+    const parseValue = parseInt(value);
+    if(!isNaN(parseValue)) {
+      this.passwordLength = parseValue;
+    }
+  }
 }
